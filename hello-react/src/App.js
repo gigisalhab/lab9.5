3be5ce3a303f7b292964258b38b4d95fdaab55b7
Jenna Salhab <salhabje@msu.edu>
2024-03-25 23:12:26 -0400

    test deploy commit

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Jenna
+          Learn React with Jenna and MSU!
         </a>
       </header>
     </div>
