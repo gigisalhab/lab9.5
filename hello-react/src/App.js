6125af89342f595c864a3490f41c087760f5b7af
Jenna Salhab <salhabje@msu.edu>
2024-04-01 22:54:57 -0400

    React ll

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,25 +1,113 @@
+import {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+const isLoggedIn = true;
+
+const magazines = [
+  { id: 1, title: 'Architectural Digest', theme: 'architecture', isAvailable: true },
+  { id: 2, title: 'Dwell', theme: 'architecture', isAvailable: true },
+  { id: 3, title: 'Communication Arts', theme: 'design', isAvailable: false },
+];
+
+
+const book = {
+  title: 'A Farewell to Arms',
+  author: 'Earnest Hemingway',
+  published: '1929',
+  image: 'https://upload.wikimedia.org/wikipedia/en/4/48/Hemingway_farewell.png',
+  width: '264',
+  height: '378'
+};
+
+function ZineRack() {
+  const listZines = magazines.map(zine =>
+    <li
+      key={zine.id}
+      style={{
+        color: zine.isAvailable ? 'red' : 'green'
+      }}
+    >
+      {zine.title}
+    </li>
+  );
+  return (
+    <ul>{listZines}</ul>
+  )
+}
+
+function Bookshelf() {
+  return (
+    <div>
+      {/* Components can't return multiple JSX tags unless they are wrapped in a parent element */}
+      {/* This is a JSX comment */}
+      <h2>{book.title} ({book.published})</h2>
+      <p>{book.author}</p>
+      {/* This a conditional that checks if an image exists before displaying it */}
+      {book.image &&
+      <img
+        className="bookCover"
+        src={book.image}
+        alt={book.title + ' cover'}
+        style={{
+          width: book.width,
+          height: book.height
+        }}
+      />
+      }
+    </div>
+  );
+}
+
+function MagicButton() {
+  const [count, setCount] = useState(0);
+  function doMagic() {
+    setCount(count + 1);
+    alert('Are you not entertained?');
+  }
+
+  return (
+    <div>
+      <h3>This is a magic button:</h3>
+      <button onClick={doMagic}>
+        Magic {count} times
+      </button>
+    </div>
+  );
+}
+
+function Divider() {
+  return (
+    <hr style={{ border: '1px solid #ccc', margin: '20px 0' }} />
+  );
+}
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Jenna and MSU!
-        </a>
+        <div>
+        {isLoggedIn ? (
+          <div>
+            <Divider />
+            <ZineRack />
+            <Divider />
+            <Bookshelf />
+            <Divider />
+            <MagicButton />
+            <Divider />
+          </div>
+        ) : (
+          <p>Please login.</p>
+        )}
+        </div>
+        
       </header>
     </div>
   );
 }
+
 
 export default App;
